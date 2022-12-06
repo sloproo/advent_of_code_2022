f5d57a288ed5f9162a6ed40f2ff64523c114f2f7
Solpo <3440817+Solpo@users.noreply.github.com>
2022-12-06 12:43:23 +0200

    Mitä itsenäisyys merkitsee sinulle?

---
+++ 06/eka.py
@@ -0,0 +1,9 @@
+with open("data.txt") as f:
+    data = f.readline()
+
+for i in range(len(data) - 3):
+    merkit = data[i:i+4]
+    kirjaimet = set([merkki for merkki in merkit])
+    if len(kirjaimet) == 4:
+        print(f"Ensimmäisen paketinalkumarkkerin viimeinen merkki on {i+4} :s merkki")
+        break
---
+++ 06/toka.py
@@ -0,0 +1,9 @@
+with open("data.txt") as f:
+    data = f.readline()
+
+for i in range(len(data) - 13):
+    merkit = data[i:i+14]
+    kirjaimet = set([merkki for merkki in merkit])
+    if len(kirjaimet) == 14:
+        print(f"Ensimmäisen viestinalkumarkkerin viimeinen merkki on {i+14} :s merkki")
+        break
