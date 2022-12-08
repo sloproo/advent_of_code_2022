104a8cef7ecf4db0bbcc2fa55f8af1870135ab3b
Solpo <3440817+Solpo@users.noreply.github.com>
2022-12-08 07:47:29 +0200

    välitilanne töihin lähtiessä

---
+++ 08/eka.py
@@ -0,0 +1,25 @@
+with open("alku.txt") as f:
+    metsa = []
+    for r in f:
+        metsa.append([int(puu) for puu in r.strip()])
+
+nakyvia = 0
+
+for y in range(len(metsa)):
+    if y == 0 or y == len(metsa) - 1:
+        continue
+    for x in range(len(metsa[y])):
+        if x == 0 or x == len(metsa[y]) - 1:
+            continue
+        if metsa[y][x] > max([puu for puu in metsa[y][:x]]):
+            nakyvia += 1
+        elif metsa[y][x] > max([puu for puu in metsa[y][x+1:]]):
+            nakyvia += 1
+        elif metsa[y][x] > max([puu for puu in metsa[:y][x]]):
+            nakyvia += 1
+        elif metsa[y][x] > max([puu for puu in metsa[y+1][x]]):
+            nakyvia += 1
+        
+
+print(f"Näkyviä puita on {nakyvia}")
+        
