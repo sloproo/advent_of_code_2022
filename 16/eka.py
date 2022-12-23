13c45ba48dd3553f9451fa5ea2fbee94790c9a69
Solpo <3440817+Solpo@users.noreply.github.com>
2022-12-24 00:04:58 +0200

    Eka toimii

--- 16/eka.py
+++ 16/eka.py
@@ -9,7 +9,7 @@ class Huone:
         return f"nimi: {self.nimi} , paine: {self.paine} , reitit: {self.reitit}\n"
 
 huoneet = {}
-with open("input.txt") as f:
+with open("data.txt") as f:
     for r in f:
         sanat = r.strip().split(" ")
         nimi = sanat[1]
@@ -23,11 +23,9 @@ venttiilit = {huoneet[huone].nimi: huoneet[huone] for huone in huoneet.keys() if
 venttiilit = dict(sorted(venttiilit.items(), key= lambda venttiili: venttiili[1].paine, reverse=True))
 # venttiilit = [huoneet[huone] for huone in huoneet.keys() if huoneet[huone].paine > 0]
 
-aikaa = 30
-sijainti = [huone for huone in huoneet.keys()][0]
+aikaa = 30  
+sijainti = "AA"
 paine = 0
-
-pass
 
 paine = apu.huoneeseen(venttiilit, huoneet, kartoitus, [], sijainti, aikaa, paine)
 print(paine)
