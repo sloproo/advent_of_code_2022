dd0c5049719f71ab8b39267f247393ec192dbc9a
sloproo <3440817+sloproo@users.noreply.github.com>
2022-12-31 08:12:02 +0200

    Siirto ja pudotus.

--- 17/eka.py
+++ 17/eka.py
@@ -1,9 +1,9 @@
 import itertools, random
 
-torni = ["#" *7]
-korkeus = 0
+torni = ["#" for _ in range(7)]
+baseline = 0
 
-def luo_palikka(muoto: str, korkeus: int) -> list:
+def luo_palikka(korkeus: int) -> list:
     palikat = {"vaaka": [(korkeus + 4, i+2) for i in range(4)],
                 "plus": [(korkeus + 5, 2), (korkeus + 5, 3), 
                         (korkeus + 5, 4), (korkeus + 6, 3), (korkeus + 4, 3)],
@@ -12,20 +12,64 @@ def luo_palikka(muoto: str, korkeus: int) -> list:
                 "pysty": [(korkeus + 4 + i, 2) for i in range(4)],
                 "nelio": [(korkeus + 4, 2), (korkeus + 5, 2),
                         (korkeus + 4, 3), (korkeus + 5, 3)]}
-    return palikat[muoto]
-
-def anna_muoto() -> str:
-    muodot = ["vaaka", "plus", "kulma", "pysty", "nelio"]
+    muodot = palikat.keys()
     i = 0
     while True:
-        yield muodot[i]
+        yield palikat[muoto]
         i += 1
         if i == 5: i = 0
 
-muotoilija = anna_muoto()
+def liikuta(palikka: list, torni: list, suunta: str, baseline: int) -> list:
+    for y, x in palikka:
+        if suunta == "<":
+            if torni[baseline + y][x-1] == "#":
+                return palikka
+            elif x == -1:
+                return palikka
+        elif suunta == ">":
+            if torni[baseline + y][x+1] == "#":
+                return palikka
+            elif x == 7:
+                return palikka
+        else:
+            raise KeyError("Mahdoton suunta liikuttaessa")
+    if suunta == "<":
+        return [(y, x -1) for y, x in palikka]
+    elif suunta == ">":
+        return [(y, x +1) for y, x in palikka]
+
+def pudota (palikka: list, torni: list, baseline: int) -> tuple[list, list]:
+    pysahtyi = False
+    for y, x in palikka:
+        if torni[y - baseline - 1][x] == "#" or y - 1 == baseline:
+            pysahtyi = True
+        
+    if pysahtyi:
+        tyhja_rivi = [" " for _ in range(7)]
+        for _ in range(4):
+            torni.append(tyhja_rivi)
+        for y, x in palikka:
+                torni[y - baseline][x] = "#"
+        for _ in range(4):
+            if torni[-1] == tyhja_rivi:
+                torni.pop[-1]
+        return (torni, [])
+    else:
+        laskenut_palikka = []
+        for y, x in palikka:
+            laskenut_palikka.append((y - 1, x))
+        return (torni, laskenut_palikka)
+        
+"""
+Baselinen määritys puuttuu
+"""
+
+muotoilija = luo_palikka()
 
 for i in range(2022):
-    palikka = luo_palikka(next(muotoilija), korkeus)
+    palikka = next(muotoilija(baseline + len(torni))
+
+
     
     """
     Tähän väliin sit siirto ja pudotus
