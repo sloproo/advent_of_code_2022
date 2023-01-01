26631195960fd60aecd54ed47f46092cac66ee65
Pyry H <3440817+sloproo@users.noreply.github.com>
2023-01-01 23:04:20 +0200

    Vaikeimmat pähkäilyt vielä edessä, mut hyvä tästä tulee

--- 19/eka.py
+++ 19/eka.py
@@ -1,6 +1,7 @@
 class Kaava:
-    def __init__(self, ore_orea: int, savi_orea: int, obs_orea: int, obs_savea:int,
-                geo_orea: int, geo_obsia: int):
+    def __init__(self, nro: int, ore_orea: int, savi_orea: int, obs_orea: int,
+                obs_savea:int, geo_orea: int, geo_obsia: int):
+                    self.nro = nro
                     self.ore_orea = ore_orea
                     self.savi_orea = savi_orea
                     self.obs_orea = obs_orea
@@ -11,12 +12,13 @@ class Kaava:
                     self.savea = 0
                     self.obsia = 0
                     self.geoja = 0
-                    self.robotit = [0, 0, 0, 0]
+                    self.robotit = [1, 0, 0, 0]
+                    self.rakentuva = None
                     self.aikaa = 26
 
     def rakenna_ore(self) -> bool:
         if self.orea >= self.ore_orea:
-            self.robotit[0] += 1
+            self.rakentuva = 0
             self.orea -= self.ore_orea
             return True
         else:
@@ -24,15 +26,15 @@ class Kaava:
 
     def rakenna_savi(self) -> bool:
         if self.orea >= self.savi_orea:
-            self.robotit[1] += 1
+            self.rakentuva = 1
             self.orea -= self.savi_orea
-            True
+            return True
         else:
             return False
     
     def rakenna_obs(self) -> bool:
         if self.orea >= self.obs_orea and self.savea >= self.obs_savea:
-            self.robotit[2] += 1
+            self.rakentuva = 2
             self.orea -= self.obs_orea
             self.savea -= self.obs_savea
             return True
@@ -41,12 +43,19 @@ class Kaava:
 
     def rakenna_geo(self) -> bool:
         if self.orea >= self.geo_orea and self.obsia >= self.geo_obsia:
-            self.robotit[3] += 1
+            self.rakentuva = 3
             self.orea -= self.geo_orea
-            self.obs -= self.geo_obsia
+            self.obsia -= self.geo_obsia
             return True
         else:
             return False
+
+    def aikaa_obsin_riittamiseen(self) -> int:
+        if (self.geo_obsia - self.obsia) % self.robotit[2] != 0:
+            aikaa_obsin_riittamiseen = (self.geo_obsia - self.obsia) // self.robotit[2] + 1
+        else:
+            aikaa_obsin_riittamiseen = (self.geo_obsia - self.obsia) // self.robotit[2]
+        return aikaa_obsin_riittamiseen
 
     def keraa(self):
         self.orea += self.robotit[0]
@@ -54,6 +63,51 @@ class Kaava:
         self.obsia += self.robotit[2]
         self.geoja += self.robotit[3]
 
+    def valmista(self):
+        if self.rakentuva == None:
+            return
+        else:
+            self.robotit[self.rakentuva] += 1
+            self.rakentuva = None
 
+    def paata_rakennettava(self):
+        if self.rakenna_geo():
+            return
+        aikaa_obsiin = self.aikaa_obsin_riittamiseen()
+        """
+        todo: funktio, joka selvittää nopeimman tavan tehdä obs-robotti
+              Ehkä rekursiivisesti funktio, joka selvittää nopeimman tavan
+              tehdä savirobotti sitä ennen (ore-robotteja alle vai suoraan savi)
+              Tai ehkä yleisluontoisempi funktio, joka antaa nopeimmin määrät
+              x ja y resursseja z ja å?
+        """
+        
 
-pass
+            
+        if self.rakenna_obs():
+            return
+        if self.rakenna_savi():
+            return
+        # self.rakenna_ore()
+
+    def kierros(self):
+        self.paata_rakennettava()
+        self.keraa()
+        self.valmista()
+        self.aikaa -= 1
+    
+    def pyorita(self) -> int:
+        while self.aikaa > 0:
+            self.kierros()
+        return self.geoja
+    
+kaavat = {}
+with open("alku.txt") as f:
+    for r in f:
+        r = r.strip().split(" ")
+        kaavan_nrot = [int(luku) for luku in [r[1][:-1], r[6], r[12], r[18], r[21],
+        r[27], r[30]]]
+        kaavat[kaavan_nrot[0]] = (Kaava(*kaavan_nrot))
+
+ekasta = kaavat[1].pyorita()
+print(ekasta)
